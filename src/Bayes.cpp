49d10aa4fa9b6cc7a1372871dabe64374be4d336
jsputnik <65297977+jsputnik@users.noreply.github.com>
2021-01-16 17:03:45 +0100

    Add files via upload

--- src/Bayes.cpp
+++ src/Bayes.cpp
@@ -2,17 +2,15 @@
 #include "Attribute.h"
 #include "Data.h"
 
-void Bayes::printAttributes() {
-    for (unsigned int i = 0; i < attributes.size(); ++i) {
-        cout << attributes[i].getWord() << ", ham: " << attributes[i].getHamCounter() << ", spam: " << attributes[i].getSpamCounter() << endl;
-    }
+Bayes::Bayes(int wordsCounter, int dataCounter) {
+    this->wordsCounter = wordsCounter;
+    this->dataCounter = dataCounter;
 }
 
-void Bayes::count(fstream& file) {
+void Bayes::loadIntoMemory(fstream& file, vector<string>& messages, int& wordsCounter, int& dataCounter) {
     string firstLine, type, msg, word;
     getline(file, firstLine, '\n'); //cleaning 1st line v1,v2
     while(loadMessage(file, type, msg)) {
-        ++dataCounter;
         for (unsigned int i = 0; i < msg.size(); ++i) {
             if (isalpha(msg[i]) || isdigit(msg[i])) {
                 word += msg[i];
@@ -24,14 +22,33 @@ void Bayes::count(fstream& file) {
                 }
             }
         }
+        messages.push_back(type + "," + msg);
+        ++dataCounter;
         type.clear();
         msg.clear();
     }
 }
 
-//load type of msg into type, text of the msg into msg
+void Bayes::printCounters() {
+     cout << "WordsCounterLearningData: " << wordsCounterLearningData << endl;
+     cout << "WordsCounter: " << wordsCounter << endl;
+     cout << "DataCounter: " << dataCounter << endl;
+}
+
+void Bayes::printAttributes() {
+    for (unsigned int i = 0; i < attributes.size(); ++i) {
+        cout << attributes[i].getWord() << ", ham: " << attributes[i].getHamCounter() << ", spam: " << attributes[i].getSpamCounter() << endl;
+    }
+}
+
+void Bayes::printTestData() {
+    for (unsigned int i = 0; i < testData.size(); ++i) {
+        testData[i].print();
+    }
+}
+
+//load type of msg into type, text of the msg into msg from file
 bool Bayes::loadMessage(fstream& file, string& type, string& msg) {
-    //string tmp;
     getline(file, type, ','); //because of structure
     if(type.empty()) {
         return false;
@@ -41,16 +58,15 @@ bool Bayes::loadMessage(fstream& file, string& type, string& msg) {
     if(msg.empty()) {
         return false;
     }
-    //msg += tmp;
     return true;
 }
 
-//load one test data
-bool Bayes::loadData(fstream& file) {
-    string type, msg;
-    if (!loadMessage(file, type, msg)) {
-        return false;
-    }
+//load one test data from string
+void Bayes::loadData(string testMessage) {
+    string msg = testMessage;
+    int typeIndex = msg.find(",");
+    string type = msg.substr(0, typeIndex);
+    msg = msg.substr(typeIndex + 1);
     Data data(type);
     string word;
     //divide into strings
@@ -62,29 +78,23 @@ bool Bayes::loadData(fstream& file) {
             if (!word.empty()) {
                 data.addWord(word);
                 word.clear();
-                //++wordsCounter;
+                //++wordsCounterLearningData;
             }
         }
     }
-    cout << "original msg: " << msg << endl;
-    data.print();
-    return true;
+    //cout << "original msg: " << msg << endl;
+    //data.print();
+    //cout << endl;
+    testData.push_back(data);
 }
 
-//load all learning data into attributes
-bool Bayes::loadAttributes(fstream& file) {
-    int k = 0;
-    //while(!file.eof()) {
-    while (k < 500) {
-        string type, msg, tmp;
-        if (!loadMessage(file, type, msg)) { //fix
-            cerr << "Error in loadMessage()" << endl;
-            return false;
-        }
-        if (type != "ham" && type != "spam") {
-            cerr << "Wrong type read in loadAttributes()" << endl;
-            return false;
-        }
+//load all learning data into attributes from strings
+void Bayes::loadAttributes(vector<string>& learningMessages) {
+    for (unsigned int k = 0; k < learningMessages.size(); ++k) {
+        string msg = learningMessages[k];
+        int typeIndex = msg.find(",");
+        string type = msg.substr(0, typeIndex);
+        msg = msg.substr(typeIndex + 1);
         string word;
         //divide into strings
         for (unsigned int i = 0; i < msg.size(); ++i) {
@@ -110,88 +120,9 @@ bool Bayes::loadAttributes(fstream& file) {
                         attributes.push_back(newAttr);
                     }
                     word.clear();
-                    //++wordsCounter;
+                    ++wordsCounterLearningData;
                 }
             }
         }
-        ++k;
     }
-    return true;
 }
-
-//void Bayes::loadAttributes()
-//{
-//    int l=0;
-//    fstream data;
-//    bool stop2 = 0;
-//    bool added = 0;
-//    data.open("spam.csv", ios::in);
-//    string type; //ham or spam in file
-//    string word, tmp1, tmp2;
-//    getline(data, type, '\n'); //cleaning 1st line
-//    //for each data
-//    while(true)
-//    {
-//        type.clear();
-//        getline(data, type, ','); //because of structure
-//        if(type.size() == 0)
-//            break;
-//        getline(data, tmp1, '\n');
-//
-//        while(!(tmp1[tmp1.size()-1] == ',' && tmp1[tmp1.size()-2] == ',' && tmp1[tmp1.size()-3] == ',')) //until ,,,
-//        {
-//            getline(data, tmp2, '\n');
-//            tmp1+=tmp2;
-//        }
-//        stringstream line(tmp1); //contains all words in data
-//
-//        while(!stop2)
-//        {
-//            added = 0;
-//            line>>word;
-//            if(word[word.size()-1] == ',' && word[word.size()-2] == ',')
-//                stop2 = 1;
-//            while(word[word.size()-1] == ',' || word[word.size()-1] == '.' || word[word.size()-1] == '\n') //remove . ,
-//            {
-//                word.pop_back();
-//            }
-//            //check if the word already occurred
-//            for(unsigned int i=0; i < attributes.size(); i++)
-//            {
-//                if(word == attributes[i].getWord())
-//                {
-//                    if(type == "spam")
-//                        attributes[i].incSpamCounter();
-//                    else
-//                        attributes[i].incHamCounter();
-//                    added = 1;
-//                    break;
-//                }
-//            }
-//            //if not in vector attributes
-//            if(!added)
-//            {
-//                Attribute newAtr;
-//                newAtr.setWord(word);
-//                if(type == "spam") {
-//                    newAtr.incSpamCounter();
-//                    attributes.push_back(newAtr);
-//                }
-//
-//                else if (type == "ham") {
-//                    newAtr.incHamCounter();
-//                    attributes.push_back(newAtr);
-//                }
-//            }
-//            incWordsCounter();
-//        }
-//        stop2 = 0;
-//        if(l%100 == 0)
-//        cout<<l<<endl;
-//        l++;
-//    }
-//    //print();
-//    data.close();
-//    cout << "wordscounter: " << wordsCounter << endl;
-//}
-
--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,75 @@ using namespace std;
 
 int main()
 {
-    Bayes x;
     int k = 7; //5572 / 7 = 796
+    vector<string> messages; //each string is a whole message including ham/spam identifier
     fstream file;
     //counting data and words
     file.open("spam.csv", ios::in);
-    x.count(file);
+    int wordsCounter = 0;
+    int dataCounter = 0;
+    Bayes::loadIntoMemory(file, messages, wordsCounter, dataCounter);
     file.close();
-    int testDataQuantity = x.getDataCounter() / k;
-    int learningDataQuantity = x.getDataCounter() - testDataQuantity;
+    int testDataQuantity = dataCounter / k;
+    int learningDataQuantity = dataCounter - testDataQuantity;
     for (int i = 0; i < k; ++i) {
-        ;
+        vector<string> testMessages;
+        vector<string> learningMessages;
+        int index = i * testDataQuantity; //starting index in messages where test data begins
+        testMessages.insert(testMessages.end(), messages.begin() + index, messages.begin() + index + testDataQuantity); //adding test data
+        learningMessages = messages;
+        learningMessages.erase(learningMessages.begin() + index, learningMessages.begin() + index + testDataQuantity); //removing test data from a copy of messages
+        Bayes x(wordsCounter, dataCounter);
+        cout << "Learning..." << endl;
+        x.loadAttributes(learningMessages);
+        cout << "Testing..." << endl;
+        for (unsigned int j = 0; j < testMessages.size(); ++j) {
+            x.loadData(testMessages[j]);
+        }
+        //x.printAttributes();
+        x.printTestData();
+/*
+        cout << "Learning messages: " << endl;
+        for (unsigned int i = 0; i < learningMessages.size(); ++i) {
+            cout << learningMessages[i] << endl;
+        }*/
+        /*
+        cout << "Test messages: " << endl;
+        for (unsigned int l = 0; l < testMessages.size(); ++l) {
+            cout << testMessages[l] << endl;
+        }*/
+        cout << "Index: " << index << endl;
+        cout << "Learning messages size: " << learningMessages.size() << endl;
+        cout << "Test messages size: " << testMessages.size() << endl;
+        cout << "Attributes quantity: " << x.getAttributes().size() << endl;
+        cout << "Test data quantity: " << x.getTestData().size() << endl;
+        x.printCounters();
+
+        /******************************************************
+        * vv comment this section if dont want any interaction
+        ******************************************************/
+        string command;
+        cout << "Next model?" << endl << "y/n/q" << endl;
+        cin >> command;
+        while (command != "y" && command != "q") {
+            command.clear();
+            cout << "Next model?" << endl << "y/n/q" << endl;
+            cin >> command;
+        }
+        if (command == "q") {
+            break;
+        }
+        /******************************************************
+        * ^^ comment this section if dont want any interaction
+        ******************************************************/
     }
-    file.open("spam.csv", ios::in);
-    string firstLine; //1st line v1,v2
-    getline(file, firstLine, '\n'); //cleaning 1st line v1,v2
-    cout << "Learning..." << endl;
-    x.loadAttributes(file);
-    x.printAttributes();
-    cout << endl;
-    int i = 0;
-    while (x.loadData(file) && i < 10/*!file.eof()*/) {
-        cout << "i: " << i << endl << endl;
-        ++i;
+
+    for (unsigned int i = 0; i < messages.size(); ++i) {
+        cout << messages[i] << endl;
     }
-    file.close();
-    cout << endl << "Words counter: " << x.getWordsCounter() << endl;
-    cout << "Data counter: " << x.getDataCounter() << endl;
+
+    cout << "Words counter: " << wordsCounter << endl;
+    cout << "Data counter: " << dataCounter << endl;
     cout << "testDataQuantity: " << testDataQuantity << endl;
     cout << "learningDataQuantity: " << learningDataQuantity << endl;
     cout << endl << "End" << endl;
